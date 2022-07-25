f6251ee7e4d3cef0c3fbe5c54ec855bf77e52127
ndakojugit <ndakoju@gmail.com>
2022-07-25 07:09:47 +0530

    castRays method

--- Car Driving Mechanics/sensor.js
+++ Car Driving Mechanics/sensor.js
@@ -1,21 +1,27 @@
 class Sensor{
     constructor(car){
         this.car = car;
-        this.rayCount = 3;
-        this.rayLength=100;
+        this.rayCount = 5;
+        this.rayLength=150;
         this.raySpread = Math.PI / 4;
 
         this.rays = [];
     }
 
     update(){
+
+        this.#castRays();
+        
+    }
+
+    #castRays(){
         this.rays = [];
         for(let i = 0; i < this.rayCount; i++){
             const rayAngle = lerp(
                 this.raySpread/2,
                 -this.raySpread/2,
-                i/(this.rayCount-1)
-            );
+                this.rayCount == 1 ? 0.5 : i/(this.rayCount-1)
+            ) + this.car.angle;
 
             const start = {x:this.car.x, y:this.car.y};
             const end = {
@@ -25,7 +31,10 @@ class Sensor{
             this.rays.push([start, end]);
 
         }
+
+
     }
+
     draw(ctx){
         for(let i =0;i<this.rayCount;i++){
             ctx.beginPath();
